fa4a1f06a5a141d06d91d58d1251b700e4f42db1
shimele <bechembayamba@gmail.com>
2020-10-16 14:56:31 +0100

    add animation

--- src/movieCard.js
+++ src/movieCard.js
@@ -3,23 +3,33 @@ import React from "react";
 export default function MovieCard(props) {
   const movie = props.movie;
   return (
-    <div className="card">
+    <div className="card" data-aos="fade-up" data-aos-duration="500">
       <img
         className="card--image"
         src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
         alt={movie.title + " poster"}
       />
       <div className="card--details">
-        <div className="card--title">
+        <div
+          className="card--title"
+          data-aos="fade-down"
+          data-aos-duration="1000"
+        >
           <h3>{movie.title}</h3>
         </div>
-        <p>
+        <p data-aos="fade-right" data-aos-duration="2000">
           <small>RELEASE DATE: {movie.release_date}</small>
         </p>
-        <p>
+        <p data-aos="fade-left" data-aos-duration="2000">
           <small>RATING: {movie.vote_average}</small>
         </p>
-        <p className="card--description">{movie.overview}</p>
+        <p
+          className="card--description"
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
+          {movie.overview}
+        </p>
       </div>
     </div>
   );
