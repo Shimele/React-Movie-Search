ac7adb5f325aeb11d7c02d3003e220346fee6922
shimele <bechembayamba@gmail.com>
2020-10-16 10:26:47 +0100

    Updates

---
+++ precache-manifest.06531f09ff96a0989165f6de6961ac01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a1adefb10a0acefdc1079e46e4a2e2",
+    "url": "/React-Movie-Search/index.html"
+  },
+  {
+    "revision": "ad00b95147a6d13e959f",
+    "url": "/React-Movie-Search/static/css/main.1501d5f3.chunk.css"
+  },
+  {
+    "revision": "3e492300631c9bfe4731",
+    "url": "/React-Movie-Search/static/js/2.5e477401.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Movie-Search/static/js/2.5e477401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad00b95147a6d13e959f",
+    "url": "/React-Movie-Search/static/js/main.d91c1fbf.chunk.js"
+  },
+  {
+    "revision": "a8023f0c7c62ff9f426c",
+    "url": "/React-Movie-Search/static/js/runtime-main.a00d71d3.js"
+  }
+]);
