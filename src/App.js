d88ed59fad39d3c689fdd856417531ca536ad1a6
shimele <bechembayamba@gmail.com>
2020-10-18 08:54:25 +0100

    add popcorn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import "./Animation.js";
 import MovieSearch from "./MovieSearch";
+import popcorn from "./popcorn.png";
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div className="container">
         <h1 className="title">Your Movie Searcher</h1>
         <MovieSearch />
+        <img className="popcorn" src={popcorn} alt="movie popcorn" />
       </div>
     );
   }
---
+++ src/images.js
@@ -0,0 +1,5 @@
+const images = [
+  { id: 1, src: "../images/popcorn.png", title: "foo", description: "bar" },
+];
+
+export default images;
